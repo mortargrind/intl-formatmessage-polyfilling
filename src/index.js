6849cbd6cce43e4f3b51409a82e4bf2a2adbf098
Semih Basmaci <mortargrind@gmail.com>
2020-03-27 12:19:46 +0100

    Intl polyfilling problem implementation.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function initApp() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <IntlProvider locale={'en'}>
+        <App/>
+      </IntlProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (!window.Intl) {
+  import('intl').then(() => initApp());
+} else {
+  initApp();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
